21439850dce1ef3883bfab708a9bf49e137d27a6
Erik Telepovský <erik.telepovsky@gmail.com>
2020-06-25 18:52:33 +0200

    item in cart

--- commerce/admin.py
+++ commerce/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from commerce.models import Cart
+
+from commerce.models import Cart, Item
+
+
+class ItemInline(admin.StackedInline):
+    model = Item
+    extra = 1
 
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user',)
+    list_display = ('id', 'user', 'items', 'total', 'created', 'modified', 'open')
     list_select_related = ['user']
+    inlines = [ItemInline]
+
+    def items(self, obj):
+        return ', '.join([str(item) for item in obj.item_set.all()])
--- commerce/models.py
+++ commerce/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
 
 from commerce import settings as commerce_settings
@@ -13,13 +16,63 @@ class AbstractProduct(models.Model):
     # discount = models.DecimalField(_(u'discount (%)'), max_digits=4, decimal_places=1, default=0)
     awaiting = models.BooleanField(_('awaiting'), default=False)
 
+    # WARNING! don't use generic relation in parent classes. Add them into child classes instead
+    # cart_items = GenericRelation('commerce.Item', related_query_name='product')
+
     class Meta:
         abstract = True
 
 
 class Cart(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    created = models.DateTimeField(_('created'), auto_now_add=True, db_index=True)
+    modified = models.DateTimeField(_('modified'), auto_now=True)
+
+    # delivery details
+    # contact details
+    # billing details
+    # shipping details
+    # discount
 
     class Meta:
         verbose_name = _('shopping cart')
         verbose_name_plural = _('shopping carts')
+
+    def __str__(self):
+        return str(self.user)
+    
+    @property
+    def total(self):
+        total = 0
+
+        for item in self.item_set.all():
+            total += item.subtotal
+
+        # TODO: + shipping
+        # TODO - discount
+        return total
+
+    @property
+    def open(self):
+        return now() - self.created
+
+
+class Item(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    product = GenericForeignKey('content_type', 'object_id')
+    quantity = models.PositiveSmallIntegerField(verbose_name=_('quantity'))
+    created = models.DateTimeField(_('created'), auto_now_add=True, db_index=True)
+    modified = models.DateTimeField(_('modified'), auto_now=True)
+
+    class Meta:
+        verbose_name = _('item')
+        verbose_name_plural = _('items')
+
+    def __str__(self):
+        return str(self.product)
+
+    @property
+    def subtotal(self):
+        return self.quantity * self.product.price
