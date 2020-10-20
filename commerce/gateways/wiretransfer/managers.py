f34562765a2369797e301fbbf71c311a5acc9fc0
Erik Telepovský <erik.telepovsky@gmail.com>
2020-10-20 21:52:03 +0200

    wiretransfer manger payment url redirects to list of orders

--- commerce/gateways/wiretransfer/managers.py
+++ commerce/gateways/wiretransfer/managers.py
@@ -1,9 +1,14 @@
 from django.template.loader import get_template
+from django.urls import reverse
+
 from commerce import settings as commerce_settings
 from commerce.managers import PaymentManager as CommercePaymentManager
 
 
 class PaymentManager(CommercePaymentManager):
+    def get_payment_url(self):
+        return reverse('commerce:orders')
+
     def render_payment_button(self):
         template = get_template('commerce/payment_button_wire_transfer.html')
         return template.render({'order': self.order})
