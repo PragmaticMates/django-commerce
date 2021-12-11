cd63168030a3583319ef47fadcdc3dc9d8c8790a
eriktelepovsky <erik.telepovsky@gmail.com>
2021-12-11 14:01:59 +0100

    v0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-commerce',
-    version='0.1.0',
+    version='0.2.0',
     description='Basic e-shop app for Django',
     long_description=open('README.md').read(),
     author='Pragmatic Mates',
